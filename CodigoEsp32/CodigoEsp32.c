30a3ae658f344ce2c2162a03e3459de7b57ca62f
João Vitor <jvdrdo@gmail.com>
2025-03-28 23:11:41 -0300

    Update CodigoEsp32.c
    Testando uma forma de conectar minha api com o esp, sem hospedar minha api

--- CodigoEsp32/CodigoEsp32.c
+++ CodigoEsp32/CodigoEsp32.c
@@ -2,6 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define SOM 33
 
@@ -15,96 +16,117 @@ const char* PASSWORD = "";
 
 HTTPClient http;
 
-const char* teste = "https://api.thingspeak.com/update?api_key=QYVMO3H6J77T93IK";
+String escrever = "https://api.thingspeak.com/update?api_key=QYVMO3H6J77T93IK";
+String ler = "https://api.thingspeak.com/channels/2897130/feeds.json?api_key=ZJB05GP2HL7JBY37";
 
 char keys[ROWS][COLS] = {
-    { '1', '2', '3', 'A' },
-    { '4', '5', '6', 'B' },
-    { '7', '8', '9', 'C' },
-    { '*', '0', '#', 'D' }
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
-uint8_t colPins[COLS] = { 18, 5, 17, 16 };
+uint8_t colPins[COLS] = { 18, 5, 17, 16 }; 
 uint8_t rowPins[ROWS] = { 23, 22, 21, 19 };
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 
 void setup() {
+  
+  Serial.begin(115200);
 
-    Serial.begin(115200);
+  WiFi.begin(SSID, PASSWORD);
 
-    WiFi.begin(SSID, PASSWORD);
+  Wire.begin(26,27);
+  lcd.init();
+  lcd.backlight();
 
-    Wire.begin(26,27);
-    lcd.init();
-    lcd.backlight();
+  pinMode(SOM, OUTPUT);
 
-    pinMode(SOM, OUTPUT);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    lcd.print(".");
+  }
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(100);
-        lcd.print(".");
-    }
-    lcd.clear();
+  lcd.clear();
 
-    lcd.print("Wifi Connected");
-    delay(2000);
-    lcd.clear();
+  lcd.print("Wifi Connected");
+  delay(2000);
+  lcd.clear();
 
-    http.begin(teste);
+  http.begin(escrever);
 
-    int httpCode = http.GET();
-    while (httpCode > 0) {
-        delay(500);
-        lcd.print(".");
-    }
-    lcd.print("API Connected");
+  int httpCode = http.GET();
+  while (httpCode > 0) {
+    delay(500);
+    lcd.print(".");
+  }
+  lcd.print("API Connected");
+ 
 
+  delay(2000);
+  lcd.clear();
 
-    delay(2000);
-    lcd.clear();
-
-    lcd.setCursor(4,0);
-    lcd.print("Digite");
-    lcd.setCursor(3,1);
-    lcd.print("sua senha:");
+  lcd.setCursor(4,0);
+  lcd.print("Digite");
+  lcd.setCursor(3,1);
+  lcd.print("sua senha:");
 }
 
-String senhaCorreta = "AB564";
 String senha ="";
 
 void conferir(){
 
-    if(senhaCorreta == senha){
-        tone(SOM, 252, 100);
-        lcd.print("Liberado");
-        noTone(SOM);
+  http.begin(escrever + "&field1=" + senha);
+
+  int httpCode = http.GET();
+
+  if(httpCode > 0){
+
+    http.begin(ler);
+
+    DynamicJsonDocument doc(2048);
+    deserializeJson(doc, http.getString());
+
+    JsonObject ultimaEntrada = doc["feeds"][doc["feeds"].size() - 1];
+
+    if(String(ultimaEntrada["field1"].as<const char*>()).getString() == "1"){
+      tone(SOM, 252, 100); 
+      lcd.print("Liberado");
+      noTone(SOM);
+      
+      http.begin(escrever + "&field2=" + 1);
 
     }else{
-        tone(SOM, 252, 100);
-        lcd.print("Senha Errada");
-        noTone(SOM);
+      tone(SOM, 252, 100);
+      lcd.print("Senha Errada");
+      noTone(SOM);
     }
+  }else{
+    lcd.print("Erro: "+String(httpCode));
+  }
+
+
 }
 
 void loop() {
-    char key = keypad.getKey();
+  char key = keypad.getKey();
+  
+  if (key != NO_KEY) {
+    if(key != '*'){
+      senha += key;
+    }else{
+      lcd.clear();
+      conferir();
+      delay(2000);
+      lcd.clear();
 
-    if (key != NO_KEY) {
-        if(key != '*'){
-            senha += key;
-        }else{
-            lcd.clear();
-            conferir();
-            delay(2000);
-            lcd.clear();
+      lcd.setCursor(4,0);
+      lcd.print("Digite");
+      lcd.setCursor(3,1);
+      lcd.print("sua senha:");
 
-            lcd.setCursor(4,0);
-            lcd.print("Digite");
-            lcd.setCursor(3,1);
-            lcd.print("sua senha:");
-
-            senha = "";
-        }
+      senha = "";
     }
+  }
 
 }
